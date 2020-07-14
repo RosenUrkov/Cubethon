a32eeb484ecc12f29740e423b2cdb382ffda8166
Rosen Urkov <rosen@telerikacademy.com>
2020-07-14 21:50:28 +0300

    Added the cubethon source code

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private int RESTART_DELAY = 1;
+
+    public GameObject completeLevelUI;
+    private bool gameHasEnded = false;
+
+    public void EndGame()
+    {
+        if(gameHasEnded)
+        {
+            return;
+        }
+
+        gameHasEnded = true;
+        Invoke("Restart", RESTART_DELAY);
+    }
+
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    private void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int FORWARD_FORCE = 2000;
+    public int MOVEMENT_FORCE = 70;
+
+
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidBody.AddForce(0, 0, FORWARD_FORCE * Time.deltaTime);
+
+        if(Input.GetKey("a"))
+        {
+            rigidBody.AddForce(-MOVEMENT_FORCE * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        } else if (Input.GetKey("d"))
+        {
+            rigidBody.AddForce(MOVEMENT_FORCE * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rigidBody.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
+    
